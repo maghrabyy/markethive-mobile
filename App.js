112a72e509608f26ea387e6f678ec35a95bfdd53
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-09-15 14:35:20 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getDoc, doc } from 'firebase/firestore';
+import { db } from './firebase';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const snapshot = await getDoc(
+        doc(db, 'Customers', '1yg1N0hkrRZZJI1eoKDhej04vGk1'),
+      );
+      console.log(snapshot.data());
+    };
+    fetchCustomers();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
