b5f3c921dc607e7247ebffb188b40cdd0e97a08b
Ahmed Eltaher <ahmedeltaher64@gmail.com>
2024-09-18 19:04:24 +0300

    Add welcome, login and register screens

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ textAlign: 'center' }}>Home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
