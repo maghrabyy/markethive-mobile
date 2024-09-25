92d3d65ecd2407962ab3689caf7428e8abb48485
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-09-25 20:39:06 +0300

    update ordersScreen loader

--- Screens/authenticatedScreens/OrdersScreen.jsx
+++ Screens/authenticatedScreens/OrdersScreen.jsx
@@ -13,98 +13,86 @@ const OrdersScreen = () => {
   const { order, isOrderLoading } = useFetchOrders();
   const navigation = useNavigation();
 
-  if (!order[0]) {
-    return (
-      <EmptyList
-        text={
-          <>
-            There are no orders here. You can go{' '}
-            <Text style={{ color: 'blue' }} onPress={() => navigation.goBack()}>
-              back
-            </Text>
-          </>
-        }
-        type="orders"
-      />
-    );
-  }
+  return isOrderLoading ? (
+    <ScreenLoader />
+  ) : !order[0] ? (
+    <EmptyList
+      text={
+        <>
+          There are no orders here. You can go{' '}
+          <Text style={{ color: 'blue' }} onPress={() => navigation.goBack()}>
+            back
+          </Text>
+        </>
+      }
+      type="orders"
+    />
+  ) : (
+    <FlatList
+      data={order}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <Card style={styles.cardContainer}>
+          <Card.Title
+            titleStyle={styles.title}
+            titleNumberOfLines={2}
+            title={`Order Placed: ${item.orderHistory[0].date
+              .toDate()
+              .toLocaleString('en-AU', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+                hour12: true,
+                minute: 'numeric',
+                hour: 'numeric',
+              })}`}
+          />
+          <Card.Content>
+            <View style={styles.content}>
+              <Text>
+                Shipping Fees:{' '}
+                <Text style={styles.textSubtitle}>
+                  {item.shippingFees === 0 ? 'Free' : item.shippingFees + 'EGP'}
+                </Text>
+              </Text>
+              <Text>
+                Total Amount:{' '}
+                <Text style={styles.textSubtitle}>
+                  {item.totalAmount.toLocaleString()} EGP
+                </Text>
+              </Text>
+              <Text>
+                Destination:{' '}
+                <Text style={styles.textSubtitle}>
+                  {item.destinationAddress.streetAddress},{' '}
+                  {item.destinationAddress.city}
+                </Text>
+              </Text>
 
-  return (
-    <>
-      {isOrderLoading ? (
-        <ScreenLoader />
-      ) : (
-        <FlatList
-          data={order}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <Card style={styles.cardContainer}>
-              <Card.Title
-                titleStyle={styles.title}
-                titleNumberOfLines={2}
-                title={`Order Placed: ${item.orderHistory[0].date
-                  .toDate()
-                  .toLocaleString('en-AU', {
-                    day: 'numeric',
-                    month: 'long',
-                    year: 'numeric',
-                    hour12: true,
-                    minute: 'numeric',
-                    hour: 'numeric',
-                  })}`}
-              />
-              <Card.Content>
-                <View style={styles.content}>
-                  <Text>
-                    Shipping Fees:{' '}
-                    <Text style={styles.textSubtitle}>
-                      {item.shippingFees === 0
-                        ? 'Free'
-                        : item.shippingFees + 'EGP'}
-                    </Text>
-                  </Text>
-                  <Text>
-                    Total Amount:{' '}
-                    <Text style={styles.textSubtitle}>
-                      {item.totalAmount.toLocaleString()} EGP
-                    </Text>
-                  </Text>
-                  <Text>
-                    Destination:{' '}
-                    <Text style={styles.textSubtitle}>
-                      {item.destinationAddress.streetAddress},{' '}
-                      {item.destinationAddress.city}
-                    </Text>
-                  </Text>
-
-                  <Text>
-                    Payment Method:{' '}
-                    <Text style={styles.textSubtitle}>
-                      {item.paymentMethod}
-                    </Text>
-                  </Text>
-                  <Text>
-                    Status:{' '}
-                    <Text style={styles.status}>
-                      {item.orderHistory[0].orderStatus}
-                    </Text>
-                  </Text>
-                </View>
-                {item.products.map((prod) => (
-                  <View key={prod.prodId}>
-                    <OrderProductInfo
-                      productId={prod.prodId}
-                      subtotal={prod.subTotal.toLocaleString()}
-                      quantity={prod.quantity}
-                    />
-                  </View>
-                ))}
-              </Card.Content>
-            </Card>
-          )}
-        />
+              <Text>
+                Payment Method:{' '}
+                <Text style={styles.textSubtitle}>{item.paymentMethod}</Text>
+              </Text>
+              <Text>
+                Status:{' '}
+                <Text style={styles.status}>
+                  {item.orderHistory[0].orderStatus}
+                </Text>
+              </Text>
+            </View>
+            {item.products.map((prod) => (
+              <View key={prod.prodId}>
+                <OrderProductInfo
+                  productId={prod.prodId}
+                  subtotal={prod.subTotal.toLocaleString()}
+                  quantity={prod.quantity}
+                />
+              </View>
+            ))}
+          </Card.Content>
+        </Card>
       )}
-    </>
+    />
   );
 };
 
